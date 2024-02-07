36cce77c128f8d7ab468262a9b7c252798b512c6
vinayk0523 <vkpaul2001@gmail.com>
2024-02-08 00:30:43 +0530

    first commit to branch

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { signController , loginController} = require('../controller/index');
+const router = express.Router();
+
+router.post('/signup', signController);
+router.post('/login',loginController);
+module.exports = router;
+
+
